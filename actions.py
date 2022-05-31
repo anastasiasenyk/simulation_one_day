3493afebd86e7b067313f960d04a302127c87107
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-06-01 02:08:35 +0300

    +random events

--- actions.py
+++ actions.py
@@ -1,11 +1,72 @@
-import random
-"""in progress"""
+import random, time
+
+
+def prime(fn):
+    def wrapper(*args, **kwargs):
+        v = fn(*args, **kwargs)
+        v.send(None)
+        return v
+    return wrapper
 
 
 class Events:
-
     def __init__(self):
-        pass
+        self.rain = self.raining()
+        self.meet = self.new_meeting()
+        self.bombing = self.bomb_attack()
 
-    def rain(self):
-        pass
+        self.current_event = self.start()
+        self.stopped = False
+
+    def send(self, char):
+        try:
+            self.current_event.send(char)
+        except StopIteration:
+            self.stopped = True
+
+    @prime
+    def start(self):
+        while True:
+            char = yield
+            if char:
+                self.current_event = random.choice([
+                    self.rain,
+                    self.meet,
+                    self.bombing
+                ])
+            else:
+                break
+
+    @prime
+    def raining(self):
+        while True:
+            char = yield
+            if char:
+                self.current_event = self.rain
+                print("It's raining, but I don't have an umbrella. Well, then I will stay at home\n")
+                time.sleep(2)
+            else:
+                break
+
+    @prime
+    def new_meeting(self):
+        while True:
+            char = yield
+            if char:
+                self.current_event = self.meet
+                print("A new unexpected meeting! The following plans are canceled!\n")
+                time.sleep(2)
+            else:
+                break
+
+    @prime
+    def bomb_attack(self):
+        while True:
+            char = yield
+            if char:
+                self.current_event = self.bombing
+                print("There was news that our city is under bomb attack, now I definitely need to go to shelter.\n"
+                      "Don't neglect air alarms!\n")
+                time.sleep(2)
+            else:
+                break
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """5 станів, між якими відбуваються переходи. Також повинно бути як мінімум 3 рандомні івенти"""
 import random
+import time
+from actions import Events
+
+
+def prime(fn):
+    def wrapper(*args, **kwargs):
+        v = fn(*args, **kwargs)
+        v.send(None)
+        return v
+    return wrapper
 
 
 class Simulation:
@@ -7,13 +17,16 @@ class Simulation:
         self.alive = True
         self.hour = 0
         self.energy = 65
+        self.once_sport = False  # once for day
+        self.once_friends = False  # once for day
 
-        self.sleep = False
-        self.study = False
-        self.sport = False
-        self.procrastination = False
-        self.friends = False
+        self.sleep = self._sleeping()
+        self.study = self._studying()
+        self.sport = self._do_sport()
+        self.procrastination = self._procrastinating()
+        self.friends = self._time_with_friends()
 
+        self.tasks = []
         self.current_state = self.alive
         self.phrases = [False, False, False]
 
@@ -25,99 +38,161 @@ class Simulation:
             print(f'-> time: {str(self.hour) if self.hour>=10 else "0"+ str(self.hour)}:00, '
                   f'action: {action}')
             if phrase:
+                time.sleep(2)
                 print(phrase)
                 print()
             print()
-            # time.sleep(1)
+            time.sleep(2)
 
     def start(self):
+        unexpected_events = Events()
         while self.alive and self.hour <= 24:
+
+            if random.randint(1, 5) == 1:
+                unexpected_events.start().send(True)
+                self.tasks.append(unexpected_events.current_event)
             self._choose_event()
+
+            if len(self.tasks) > 1:
+                if self.tasks[1] == self.study:
+                    continue
+                elif self.tasks[0] == unexpected_events.meet:
+                    self.tasks = []
+                    self.tasks.append(self._time_with_friends())
+                    self.tasks.append(unexpected_events.meet)
+                elif self.tasks[0] == unexpected_events.rain:
+                    if self.tasks[1] != self.friends or self.tasks != self.sport:
+                        self.tasks.pop()
+                        self.tasks.append(random.choice([self._studying(), self._procrastinating()]))
+                elif self.tasks[0] == unexpected_events.bombing:
+                    self.tasks.pop()
+                    self.tasks.pop().send(True)
+                    print(f'-> time: {str(self.hour) if self.hour>=10 else "0"+ str(self.hour)}:00')
+                    print()
+                    self._change_time()
+                else:
+                    self.tasks.pop()
+
+                while self.tasks:
+                    self.tasks.pop().send(True)
+            else:
+                self.tasks.pop().send(True)
         return False
 
     def _choose_event(self):
         while True:
             number = random.randint(0, 10)
             if self.hour <= 4:
-                if number <= 7 and self.energy <= 50:
-                    return self._sleeping()
+                if number <= 7 and self.energy <= 70:
+                    return self.tasks.append(self._sleeping())
                 elif number <= 9 and self.energy >= 35:
-                    return self._studying()
+                    return self.tasks.append(self._studying())
                 else:
-                    return self._procrastinating()
+                    return self.tasks.append(self._procrastinating())
             elif self.hour <= 9:
-                if number <= 5:
-                    return self._studying()
+                if number <= 9:
+                    return self.tasks.append(self._studying())
                 else:
-                    return self._procrastinating()
+                    return self.tasks.append(self._procrastinating())
             elif self.hour <= 15:
                 if number <= 3 and self.energy >= 20:
-                    return self._studying()
-                elif number <= 5:
-                    return self._procrastinating()
+                    return self.tasks.append(self._studying())
+                elif number <= 4:
+                    return self.tasks.append(self._procrastinating())
                 else:
-                    if self.energy >= 80 and not self.friends:
-                        return self._time_with_friends()
+                    if self.energy >= 80 and not self.once_friends:
+                        return self.tasks.append(self._time_with_friends())
             elif self.hour <= 18:
-                if number <= 5 and not self.sport:
-                    return self._do_sport()
+                if number <= 5 and not self.once_sport:
+                    return self.tasks.append(self._do_sport())
                 elif number <= 7:
-                    if self.energy >= 70 and not self.friends:
-                        return self._time_with_friends()
+                    if self.energy >= 70 and not self.once_friends:
+                        return self.tasks.append(self._time_with_friends())
                 else:
-                    return random.choice([self._studying(), self._procrastinating()])
+                    return self.tasks.append(random.choice([self._studying(), self._procrastinating()]))
             elif self.hour <= 21:
                 if number <= 8:
-                    return self._studying()
+                    return self.tasks.append(self._studying())
                 else:
-                    return self._procrastinating()
+                    return self.tasks.append(self._procrastinating())
             else:
                 if number <= 7:
-                    return self._studying()
+                    return self.tasks.append(self._studying())
                 elif number <= 9:
-                    return self._procrastinating()
+                    return self.tasks.append(self._procrastinating())
                 else:
-                    return self._sleeping()
+                    return self.tasks.append(self._sleeping())
 
+    @prime
     def _sleeping(self):
-        self.sleep = True
-        for index in range(random.randint(4, 9)):
-            self._user_interface('sleeping')
-            self._change_time()
-            self._change_energy(22)
-        self.sleep = False
+        while True:
+            char = yield
+            if char:
+                self.sleep = True
+                for index in range(random.randint(4, 9)):
+                    self._user_interface('sleeping')
+                    self._change_time()
+                    self._change_energy(22)
+                self.sleep = False
+            else:
+                break
 
+    @prime
     def _studying(self):
-        self._user_interface(random.choice([
-            'doing OP',
-            'coding this laboratory',
-            'meticulously studying a course from block «Svitohlyadne yadro»',
-            'studying',
-            'reading about CB-therapy',
-            'preparation for the exam',
-            'reading a book'
-        ]))
-        self._change_time()
-        self._change_energy(-7)
+        while True:
+            char = yield
+            if char:
+                self._user_interface(random.choice([
+                    'doing OP',
+                    'coding this laboratory',
+                    'meticulously studying a course from block «Svitohlyadne yadro»',
+                    'studying',
+                    'reading about CB-therapy',
+                    'preparation for the exam',
+                    'reading a book'
+                ]))
+                self._change_time()
+                self._change_energy(-7)
+            else:
+                break
 
+    @prime
     def _procrastinating(self):
-        self._user_interface('procrastinating')
-        self._change_time()
-        self._change_energy(-3)
+        while True:
+            char = yield
+            if char:
+                self._user_interface('procrastinating')
+                self._change_time()
+                self._change_energy(-3)
+            else:
+                break
 
+    @prime
     def _do_sport(self):
-        self.sport = True
-        for index in range(random.randint(1, 2)):
-            self._user_interface('MMA time!!')
-            self._change_time()
-            self._change_energy(-15)
+        while True:
+            char = yield
+            if char:
+                self.once_sport = True
+                for index in range(random.randint(1, 2)):
+                    self._user_interface('MMA time!!')
+                    self._change_time()
+                    self._change_energy(-15)
+            else:
+                break
 
+    @prime
     def _time_with_friends(self):
-        self.friends = True
-        for index in range(random.randint(2, 3)):
-            self._user_interface('spending time with friends', 'friends? finally' if self.phrases[1] else None)
-            self._change_time()
-            self._change_energy(-1)
+        while True:
+            char = yield
+            if char:
+                self.once_friends = True
+                for index in range(random.randint(1, 2)):
+                    self._user_interface('spending time with friends',
+                                         'friends? finally' if self.phrases[1] else None)
+                    self._change_time()
+                    self._change_energy(-1)
+            else:
+                break
 
     def _change_time(self):
         if self.alive:
@@ -129,8 +204,8 @@ class Simulation:
         if self.alive and self.energy >= 100 and number > 0:
             if not self.phrases[0]:
                 self.phrases[0] = True
-                print('you will soon explode from that energy, be calmer')
-                print()
+                print("Sleeping Beauty\n")
+                time.sleep(2)
         else:
             self.energy += number
         if self.energy <= 0:
